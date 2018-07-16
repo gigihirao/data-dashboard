1b58690c97efdb66f71056ef3457325def082585
Alyne Martins <martins.alyne@gmail.com>
2018-07-16 17:10:11 -0300

    iniciando o JS, likando sedes com a pagina turma.html

--- js/app.js
+++ js/app.js
@@ -1,6 +1,39 @@
-/*
- * Funcionalidad de tu producto
- */
+//var arequipa = document.getElementById("arequipa").textContent;
+//var mexico = document.getElementById("mexico").textContent;
+//var lima = document.getElementById("lima").textContent;
+//var santiago = document.getElementById("santiago").textContent;
 
-// Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+//
+//
+//var sede = arequipa.addEventListener('click', classes(sede));
+//mexico.addEventListener('click', teste.innerHTML = "Cidade do Mexico");
+//lima.addEventListener('click', classes(lima));
+//santiago.addEventListener('click', classes(santiago));
+
+
+function classes(sede) {
+	window.location.href = "turma.html";
+	var nameSede = sede.textContent;
+	var tituloSede = document.querySelector('#nameSede');
+	tituloSede.innerHTML = nameSede;
+	console.log(tituloSede);
+	console.log(nameSede);
+
+	if (nameSede == "AREQUIPA") {
+		//		chamar a turma no data com graficos
+
+	}
+	if (nameSede == "CIDADE DO MÉXICO") {
+		//		chamar a turma no data com graficos
+
+	}
+	if (nameSede == "LIMA") {
+		//		chamar a turma no data com graficos
+
+	}
+	if (nameSede == "SANTIAGO") {
+
+		//		chamar a turma no data com graficos
+	}
+
+}
