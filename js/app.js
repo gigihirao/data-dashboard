c02b14985602810e4a4e9f1ee3a54c30ddc409a5
gigihirao <gigihirao@gmail.com>
2018-07-17 01:35:27 -0300

    graficos de pizza das medias adicionados

--- js/app.js
+++ js/app.js
@@ -1,30 +1,3 @@
-var sede = window.location.hash;
-var tituloSede = document.getElementById("nameSede");
-
-
-if (sede == "#AQP") {
-	tituloSede.innerHTML = "AREQUIPA";
-	classes(AQP);
-}
-if (sede == "#CDMX") {
-	tituloSede.innerHTML = "CIDADE DO MÉXICO";
-	classes(CDMX);
-}
-if (sede == "#LIM") {
-	tituloSede.innerHTML = "LIMA";
-	classes(LIM);
-}
-if (sede == "#SCL") {
-	tituloSede.innerHTML = "SANTIAGO";
-	classes(SCL);
-}
-
-
-
-function classes(sede) {
-
-}
-
 const sedes = {
 	AQP: "AQUERIPA",
 	CDMX: "CIDADE DO MÉXICO",
@@ -42,18 +15,85 @@ for (var turma in data[sede]) {
 	select.appendChild(option);
 }
 
+var techPassingScore = 1800 * 70 / 100;
+var hsePassingScore = 1200 * 70 / 100;
+
 
 function changeTurma() {
 	// select.value --> turma selecionada
 	// criar os graficos aqui dentro
-	var ctx = document.getElementsByClassName("line-chart");
-	//type, data, option
-	var chartGraph = new Chart(ctx, {
-		type: 'line',
-		data: {
-			labels: [sprint1, sprint2],
-		}
+	// var ctx = document.getElementsByClassName("line-chart");
+	// //type, data, option
+	// var chartGraph = new Chart(ctx, {
+	// 	type: 'line',
+	// 	data: {
+	// 		labels: [sprint1, sprint2],
+	// 	}
 
+	// });
+
+	var npsAverage = document.getElementById("nps-average-value");
+	var nps = data[sede][select.value]["ratings"];
+	var sum = nps.reduce(function(total, sprint){
+		return sprint["nps"]["promoters"] - sprint["nps"]["detractors"] + total;
+	}, 0);
+
+	var mean = sum / nps.length;
+	npsAverage.textContent = mean;
+
+	//non active students
+	var nonActiveCtx = document.getElementById("non-active-students").getContext("2d");
+	var studentsTotal = data[sede][select.value]["students"].length;
+	var nonActiveTotal = data[sede][select.value]["students"].reduce(function(total, student){
+		return total + !student["active"];
+	}, 0);
+	var nonActiveData = [studentsTotal - nonActiveTotal, nonActiveTotal];
+	new Chart(nonActiveCtx, {
+		type: "pie",
+		data: {
+			labels: ["Active", "Non-Active"],
+			datasets: [{
+				data: nonActiveData,
+				backgroundColor: ["green", "pink"],
+			}]
+		},
 	});
 
+	//tech
+	var passingTechStudentsCtx = document.getElementById("passing-students-tech").getContext("2d");
+	var passingStudentsTotal = data[sede][select.value]["students"].reduce(function(total, student){
+		var average = student["sprints"].reduce(function(total, sprint){
+			return total + sprint["score"]["tech"];
+		}, 0) / student["sprints"].length;
+		return total + (average >= techPassingScore);
+	}, 0);
+	new Chart(passingTechStudentsCtx, {
+		type: "pie",
+		data: {
+			labels: ["Passing Tech", "Non-Passing Tech"],
+			datasets: [{
+				data: [passingStudentsTotal, studentsTotal - passingStudentsTotal],
+				backgroundColor: ["green", "pink"],
+			}]
+		},
+	});
+
+	//hse
+	var passingHseStudentsCtx = document.getElementById("passing-students-hse").getContext("2d");
+	var passingStudentsTotalHse = data[sede][select.value]["students"].reduce(function(total, student){
+		var average = student["sprints"].reduce(function(total, sprint){
+			return total + sprint["score"]["hse"];
+		}, 0) / student["sprints"].length;
+		return total + (average >= hsePassingScore);
+	}, 0);
+	new Chart(passingHseStudentsCtx, {
+		type: "pie",
+		data: {
+			labels: ["Passing HSE", "Non-Passing HSE"],
+			datasets: [{
+				data: [passingStudentsTotalHse, studentsTotal - passingStudentsTotalHse],
+				backgroundColor: ["green", "pink"],
+			}]
+		},
+	});
 }
